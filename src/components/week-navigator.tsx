08b6f26bc42148046e87c09a3511b649ad966864
pawelkom88 <pawel.komorkiewicz@yahoo.co.uk>
2025-01-11 21:16:33 +0000

    fix bugs

--- src/components/week-navigator.tsx
+++ src/components/week-navigator.tsx
@@ -1,29 +1,28 @@
-import { format, addDays, startOfWeek } from 'date-fns'
+import { format } from "date-fns";
+import { getWeekDays } from "@/utils/helpers.ts";
+import { DATE_PATTERN } from "@/utils/constants.ts";
 
-interface WeekNavigatorProps {
-  currentDate: Date
+interface Props {
+  workoutDate: Date;
 }
 
-export function WeekNavigator({ currentDate }: WeekNavigatorProps) {
-  const weekStart = startOfWeek(currentDate)
-  const weekDays = Array.from({ length: 7 }).map((_, i) => addDays(weekStart, i))
-
+export function WeekNavigator({ workoutDate }: Props) {
   return (
     <div className="grid grid-cols-7 gap-2 text-center">
-      {weekDays.map((day) => (
+      {getWeekDays(workoutDate).map((day) => (
         <div
           key={day.toISOString()}
           className={`p-2 rounded-lg ${
-            format(day, 'yyyy-MM-dd') === format(currentDate, 'yyyy-MM-dd')
-              ? 'bg-blue-100 font-semibold'
-              : ''
+            format(day, DATE_PATTERN.THREE_LETTERS) ===
+            format(workoutDate, DATE_PATTERN.THREE_LETTERS)
+              ? "bg-teriary text-contrastReversed font-semibold"
+              : ""
           }`}
         >
-          <div className="text-sm text-gray-500">{format(day, 'EEE')}</div>
-          <div className="text-lg">{format(day, 'd')}</div>
+          <div className="text-sm text-contrast">{format(day, "EEE")}</div>
+          <div className="text-lg text-contrast">{format(day, "d")}</div>
         </div>
       ))}
     </div>
-  )
+  );
 }
-
